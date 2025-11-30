2f2dafb6c3f72372ef7d85f0ae499afbb435bc3a
Muneeb ur Rehman Memon <muneeb20157@gmail.com>
2025-11-30 15:17:21 +0500

    Refactor main.cpp for screen size and code clarity

--- src/main.cpp
+++ src/main.cpp
@@ -1,84 +1,106 @@
 #include <raylib.h>
 #include <stdlib.h>
 #include <math.h>
-
-int main() {
-    const int screenWidth = 840;
-    const int screenHeight = 960;
+#include <string>
+int main()
+{
+    const int screenWidth = 1000;
+    const int screenHeight = 640;
     const int cellSize = 40;
-    const int gridWidth = screenWidth / cellSize;
-    const int gridHeight = screenHeight / cellSize;
+    const int gridWidth = screenWidth / cellSize;   // find the number of grid call horizantally
+    const int gridHeight = screenHeight / cellSize; // find the number of grid call verically
+    int score = 0;
 
     int snakeLength = 4;
-    int snakeX = screenWidth/20;
+    int snakeX = screenWidth / 20;
     int snakeY = screenHeight / 20;
     int snakePosition[255][2] = {{snakeX, snakeY}};
-    
+
     char key = 'R';
     Color black = {0, 0, 0, 255};
     Color white = {255, 255, 255, 255};
     Color red = {255, 73, 92, 255};
-    int foodX=0;
-    int foodY=0;
+    int foodX = 0;
+    int foodY = 0;
     InitWindow(screenWidth, screenHeight, "first game window by umais using raylib");
     SetTargetFPS(10);
 
-    do {
+    do
+    {
         foodX = GetRandomValue(0, gridWidth - 1);
         foodY = GetRandomValue(0, gridHeight - 1);
     } while (foodX == snakeX && foodY == snakeY);
 
-    while (!WindowShouldClose()) {
-        if (IsKeyDown(KEY_RIGHT)) key = 'R';
-        if (IsKeyDown(KEY_LEFT)) key = 'L';
-        if (IsKeyDown(KEY_UP)) key = 'U';
-        if (IsKeyDown(KEY_DOWN)) key = 'D';
+    while (!WindowShouldClose())
+    {
+        if (IsKeyDown(KEY_RIGHT) && key != 'L')
+            key = 'R';
+        if (IsKeyDown(KEY_LEFT) && key != 'R')
+            key = 'L';
+        if (IsKeyDown(KEY_UP) && key != 'D')
+            key = 'U';
+        if (IsKeyDown(KEY_DOWN) && key != 'U')
+            key = 'D';
 
-        for(int i=snakeLength; i>0; i--) { //move body segments 
-                snakePosition[i][0] = snakePosition[i-1][0];
-                snakePosition[i][1] = snakePosition[i-1][1];
-            }
+        for (int i = snakeLength; i > 0; i--)
+        { // move body segments
+            snakePosition[i][0] = snakePosition[i - 1][0];
+            snakePosition[i][1] = snakePosition[i - 1][1];
+        }
 
-        if (key == 'R') snakePosition[0][0] +=1; //move head right
+        if (key == 'R')
+            snakePosition[0][0] += 1; // move head right
 
-        if (key == 'L') snakePosition[0][0] -= 1; //move head left
-        
-        if (key == 'U') snakePosition[0][1] -= 1; //move head up
-        
-        if (key == 'D') snakePosition[0][1] += 1; //move head Down
-        
+        if (key == 'L')
+            snakePosition[0][0] -= 1; // move head left
 
-        if (snakePosition[0][0] > gridWidth) snakePosition[0][0] = 0;
-        if (snakePosition[0][0] < 0) snakePosition[0][0] = gridWidth;
-        if (snakePosition[0][1] > gridHeight) snakePosition[0][1] = 0;
-        if (snakePosition[0][1] < 0) snakePosition[0][1] = gridHeight;
+        if (key == 'U')
+            snakePosition[0][1] -= 1; // move head up
+
+        if (key == 'D')
+            snakePosition[0][1] += 1; // move head Down
+
+        if (snakePosition[0][0] > gridWidth - 1)
+            snakePosition[0][0] = 0;
+        if (snakePosition[0][0] < 0)
+            snakePosition[0][0] = gridWidth;
+        if (snakePosition[0][1] > gridHeight - 1)
+            snakePosition[0][1] = 0;
+        if (snakePosition[0][1] < 0)
+            snakePosition[0][1] = gridHeight;
 
         // float dx = snakeX - foodX;
         // float dy = snakeY - foodY;
         // float distance = sqrt(dx*dx + dy*dy);
 
-        if (snakePosition[0][0] == foodX && snakePosition[0][1] == foodY ) { 
-            //distance <= playerRadius + cellSize/2
-            do {
+        if (snakePosition[0][0] == foodX && snakePosition[0][1] == foodY)
+        {
+            // distance <= playerRadius + cellSize/2
+            do
+            {
                 foodX = GetRandomValue(0, gridWidth - 1);
                 foodY = GetRandomValue(0, gridHeight - 1);
                 snakeLength++;
+                score += 10;
             } while (snakePosition[0][0] == foodX && snakePosition[0][1] == foodY);
         }
-
+        std::string scoretext = "Score: " + std::to_string(score);
         BeginDrawing();
         ClearBackground(black);
-        for(int i=0; i<gridWidth; i++) {
-            for(int j=0; j<gridHeight; j++) {
-                DrawRectangleLines(i*cellSize, j*cellSize, cellSize, cellSize, GRAY);
+        for (int i = 0; i < gridWidth; i++)
+        {
+            for (int j = 0; j < gridHeight; j++)
+            {
+                DrawRectangleLines(i * cellSize, j * cellSize, cellSize, cellSize, GRAY);
             }
         }
-        DrawRectangle(foodX*cellSize, foodY*cellSize, cellSize,cellSize ,red);
-        for(int i=0; i<snakeLength; i++) {
-            DrawRectangle((snakePosition[i][0])*cellSize,(snakePosition[i][1])*cellSize, cellSize,cellSize, white);
+        DrawText(scoretext.c_str(), screenWidth - 120, 20, 20, WHITE);
+        DrawRectangle(foodX * cellSize, foodY * cellSize, cellSize, cellSize, red);
+        for (int i = 0; i < snakeLength; i++)
+        {
+            DrawRectangle((snakePosition[i][0]) * cellSize, (snakePosition[i][1]) * cellSize, cellSize, cellSize, white);
         }
         EndDrawing();
-    
     }
     CloseWindow();
     return 0;
